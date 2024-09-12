88da950442ddbd121e9baf31e1fb3c57f5d5cb39
John Cook <coder-a599-7927@johncook.co.uk>
2024-09-12 10:28:04 +0100

    Add BIP85/Shake256Stream extended determinism test

--- Tests/BIPTests/Bip85Tests.cs
+++ Tests/BIPTests/Bip85Tests.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -159,6 +160,93 @@ namespace uk.JohnCook.dotnet.LTOEncryptionManager.Tests.BIPTests
 				// Read 30 bytes into drngEntropy[50..]
 				shake256Stream.Read(drngEntropy, 50, 30);
 				Assert.AreEqual(testVector.DrngEntropyHex, Utils.ByteEncoding.ToHexString(drngEntropy));
+				if (Debugger.IsAttached)
+				{
+					semaphore.Release();
+				}
+			});
+		}
+
+
+		[TestMethod]
+		public async Task ExtendedDeterminismTest()
+		{
+			IEnumerable<Models.Bip85DrngEntropyTestVector>? testVectors = await GetDrngEntropyTestVectorsAsync().ConfigureAwait(false);
+			Assert.IsNotNull(testVectors);
+			using SemaphoreSlim semaphore = new(1);
+			_ = Parallel.ForEach(testVectors, testVector =>
+			{
+				if (Debugger.IsAttached)
+				{
+					semaphore.Wait();
+				}
+				Bip32Node rootNode = GetBip32RootNode(testVector.MasterNodePrivateKey);
+				Bip32Node? derivationNode = GetBip32NodeFromDerivationPath(rootNode, testVector.DerivationPath);
+				Assert.IsNotNull(derivationNode);
+				Assert.IsFalse(derivationNode.IsMasterNode);
+				Assert.AreEqual(testVector.DerivationPath, derivationNode.DerivationPath);
+				Assert.IsNotNull(derivationNode.PrivateKeySerialised);
+				Assert.AreEqual(testVector.PrivateKeyHex, new(Utils.ByteEncoding.ToHexString([.. derivationNode.Left])));
+				ReadOnlySpan<byte> entropyFromK = Bip85.GetEntropy(derivationNode, 64);
+				Assert.AreEqual(testVector.EntropyHex, Utils.ByteEncoding.ToHexString([.. entropyFromK]));
+
+				using Shake256DRNG shake256DRNG = new(derivationNode);
+				using Shake256Stream shake256Stream = new(shake256DRNG);
+				shake256Stream.SetStreamLength(0);
+				using IncrementalHash hasher = IncrementalHash.CreateHash(HashAlgorithmName.SHA512);
+				int bufferSize = 32768;
+				byte[] buffer = new byte[bufferSize];
+				// Test for 1 KiB, 1 MiB, 5 MiB, 1 GiB
+				long[] bytesToCheckArray = [
+					80,
+					1 * (1 << 10),
+					1 * (1 << 20),
+					5 * (1 << 20),
+					1 * (1 << 30)
+					];
+				string[] expectedHashes = [
+					testVector.DrngEntropy80BSha512Hash,
+					testVector.DrngEntropy1KiBSha512Hash,
+					testVector.DrngEntropy1MiBSha512Hash,
+					testVector.DrngEntropy5MiBSha512Hash,
+					testVector.DrngEntropy1GiBSha512Hash
+					];
+				Assert.AreEqual(bytesToCheckArray.Length, expectedHashes.Length);
+
+				for (int i = 0; i < bytesToCheckArray.Length; i++)
+				{
+					long bytesToCheck = bytesToCheckArray[i];
+					string expectedHash = expectedHashes[i];
+					try
+					{
+						long startPosition = shake256Stream.Position;
+						long stillToRead = bytesToCheck;
+						CancellationToken token = new(false);
+						Assert.IsTrue(shake256Stream.CanRead);
+						shake256Stream.ExtendStreamLength(bytesToCheck);
+						Assert.IsTrue(shake256Stream.Position < shake256Stream.Length);
+						while (stillToRead > 0)
+						{
+							Assert.IsTrue(shake256Stream.CanRead);
+							Assert.IsTrue(shake256Stream.Position < shake256Stream.Length);
+							int bytesToRead = stillToRead > bufferSize ? bufferSize : (int)stillToRead;
+							int bytesRead = shake256Stream.Read(buffer, 0, bytesToRead);
+							hasher.AppendData(buffer, 0, bytesRead);
+							stillToRead -= bytesRead;
+						}
+						byte[] hash = new byte[SHA512.HashSizeInBytes];
+						hasher.GetHashAndReset(hash);
+						Assert.AreEqual(shake256Stream.Length, shake256Stream.Position);
+						string hashString = Utils.ByteEncoding.ToHexString(hash);
+						Assert.AreEqual(expectedHash, hashString);
+						Trace.WriteLine($"Path: {testVector.DerivationPath}, byte[{startPosition}..{shake256Stream.Position}] ({bytesToCheck} bytes wanted from DRNG), SHA2-512 Hash: {hashString}");
+					}
+					catch (Exception)
+					{
+						throw;
+					}
+				}
+
 				if (Debugger.IsAttached)
 				{
 					semaphore.Release();
--- Tests/Models/Bip85DrngEntropyTestVector.cs
+++ Tests/Models/Bip85DrngEntropyTestVector.cs
@@ -12,11 +12,27 @@ namespace uk.JohnCook.dotnet.LTOEncryptionManager.Tests.Models
 
 	public class Bip85DrngEntropyTestVector : Collection<string>
 	{
+		[JsonIgnore]
 		public string MasterNodePrivateKey => this[0];
+		[JsonIgnore]
 		public string DerivationPath => this[1];
+		[JsonIgnore]
 		public string PrivateKeyHex => this[2].ToUpperInvariant();
+		[JsonIgnore]
 		public string EntropyHex  => this[3].ToUpperInvariant();
+		[JsonIgnore]
 		public int RequestedEntropyBytes => int.Parse(this[4], NumberStyles.None, CultureInfo.InvariantCulture);
+		[JsonIgnore]
 		public string DrngEntropyHex => this[5].ToUpperInvariant();
+		[JsonIgnore]
+		public string DrngEntropy80BSha512Hash => this[6].ToUpperInvariant();
+		[JsonIgnore]
+		public string DrngEntropy1KiBSha512Hash => this[7].ToUpperInvariant();
+		[JsonIgnore]
+		public string DrngEntropy1MiBSha512Hash => this[8].ToUpperInvariant();
+		[JsonIgnore]
+		public string DrngEntropy5MiBSha512Hash => this[9].ToUpperInvariant();
+		[JsonIgnore]
+		public string DrngEntropy1GiBSha512Hash => this[10].ToUpperInvariant();
 	}
 }
