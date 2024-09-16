702a3e001f3f2278556e306d674bb13a6be82e75
John Cook <coder-a599-7927@johncook.co.uk>
2024-09-16 11:52:40 +0100

    Add BigInteger IsFermatNumber extension

---
+++ Tests/MathsTests/FermatNumberTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Numerics;
+using System.Threading.Tasks;
+using uk.JohnCook.dotnet.LTOEncryptionManager.Utils.Maths;
+
+namespace uk.JohnCook.dotnet.LTOEncryptionManager.Tests.MathsTests
+{
+	public partial class MathsTests
+	{
+		[TestMethod]
+		public void FermatNumberTest()
+		{
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => _ = BigIntegerExtensions.TryGetFermatNumberForBitLength(-1, out _));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => _ = BigIntegerExtensions.TryGetFermatNumberForBitLength(0, out _));
+			Dictionary<int, BigInteger?> fermatNumberBitLengthDictionary = new()
+			{
+				{1, null},
+				{2, 3 },
+				{3, 5},
+				{4, null},
+				{5, 17},
+				{6, null},
+				{7, null},
+				{8, null},
+				{9, 257},
+				{17, 65537},
+				{32, null},
+				{33, 4294967297},
+				{64, null},
+				{65, BigInteger.Parse("18446744073709551617", NumberStyles.None, CultureInfo.InvariantCulture)},
+				{129, BigInteger.Parse("340282366920938463463374607431768211457", NumberStyles.None, CultureInfo.InvariantCulture)},
+				{257, BigInteger.Parse("115792089237316195423570985008687907853269984665640564039457584007913129639937", NumberStyles.None, CultureInfo.InvariantCulture)},
+				{513, BigInteger.Parse("13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084097", NumberStyles.None, CultureInfo.InvariantCulture)}
+			};
+
+			_ = Parallel.ForEach(fermatNumberBitLengthDictionary, dictionaryEntry =>
+			{
+				bool expectedResult = dictionaryEntry.Value != null;
+				bool result = BigIntegerExtensions.TryGetFermatNumberForBitLength(dictionaryEntry.Key, out BigInteger? fermatNumber);
+				Assert.AreEqual(expectedResult, result);
+				Assert.AreEqual(dictionaryEntry.Value, fermatNumber);
+				BigInteger testNumber = fermatNumber ?? BigInteger.Pow(2, dictionaryEntry.Key) - 1;
+				bool result2 = testNumber.IsFermatNumber();
+				Assert.AreEqual(expectedResult, result2);
+				if (result)
+				{
+					Trace.WriteLine($"Bit length {dictionaryEntry.Key} result was {result}: {fermatNumber}");
+				}
+			});
+		}
+	}
+}
---
+++ Utils/Maths/BigIntegerExtensions/IsFermatNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace uk.JohnCook.dotnet.LTOEncryptionManager.Utils.Maths
+{
+	public static partial class BigIntegerExtensions
+	{
+		/// <summary>
+		/// Checks if a given number is a Fermat number.
+		/// </summary>
+		/// <param name="number">The number to check.</param>
+		/// <returns><see langword="true"/> if <paramref name="number"/> is a Fermat number; otherwise, <see langword="false"/>.</returns>
+		/// <remarks>
+		/// <para>See also: <seealso cref="TryGetFermatNumberForBitLength(int, out BigInteger?)"/>.</para>
+		/// </remarks>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="number"/>'s bit length is less than 1
+		///  or exceeds <see cref="int.MaxValue"/>.</exception>
+		public static bool IsFermatNumber(this BigInteger number)
+		{
+			ArgumentOutOfRangeException.ThrowIfGreaterThan(number.GetBitLength(), int.MaxValue, nameof(number.GetBitLength));
+			bool fermatNumberForBitLength = TryGetFermatNumberForBitLength((int)number.GetBitLength(), out BigInteger? fermatNumber);
+			return fermatNumberForBitLength && number == fermatNumber;
+		}
+
+		/// <summary>
+		/// Calculates a Fermat number that fits within <paramref name="bitLength"/> bits.
+		/// </summary>
+		/// <param name="bitLength">The bit length of the requested Fermat number.</param>
+		/// <param name="fermatNumber">The calculated Fermat number, if one exists that is exactly <paramref name="bitLength"/> bits long.</param>
+		/// <returns><see langword="true"/> if the calculated <paramref name="fermatNumber"/> is exactly <paramref name="bitLength"/> bits long, otherwise <see langword="false"/>.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="bitLength"/> is less than 1.</exception>
+		public static bool TryGetFermatNumberForBitLength(int bitLength, [NotNullWhen(true)] out BigInteger? fermatNumber)
+		{
+			// Negative and zero bit lengths are invalid
+			ArgumentOutOfRangeException.ThrowIfNegativeOrZero(bitLength, nameof(bitLength));
+
+			// 0b0 (0) and 0b1 (1) are not Fermat numbers
+			if (bitLength == 1)
+			{
+				fermatNumber = null;
+				return false;
+			}
+			// 0b10 (2) is not a Fermat number, whilst 0b11 (3) is a Fermat number
+			else if (bitLength == 2)
+			{
+				fermatNumber = new(3);
+				return true;
+			}
+
+			// Calculate 2^(2^n)+1
+			fermatNumber = 2;
+			for (long i = bitLength; i > 1; i /= 2)
+			{
+				fermatNumber = BigInteger.Pow(fermatNumber.Value, 2);
+			}
+			fermatNumber += 1;
+
+			// Only set the Fermat number and return true if the bit length is correct
+			if (fermatNumber.Value.GetBitLength() != bitLength)
+			{
+				fermatNumber = null;
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+}
