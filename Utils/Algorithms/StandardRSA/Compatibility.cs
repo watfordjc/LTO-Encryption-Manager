27d5ac5787a7beacdd2382c0b437a33115005989
John Cook <coder-a599-7927@johncook.co.uk>
2024-09-17 19:59:51 +0100

    Add BigPrime and StandardRSA.Compatibility

---
+++ Utils/Algorithms/StandardRSA/Compatibility.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace uk.JohnCook.dotnet.LTOEncryptionManager.Utils.Algorithms
+{
+	public static partial class StandardRSA
+	{
+		/// <summary>
+		/// Flags, and combinations of flags, used to change how deterministic prime number and asymmetric key generation is performed.
+		/// </summary>
+		/// <remarks>
+		/// <para>Other implementations of BIP85, and their dependencies, may behave differently or change how they function.</para>
+		/// <para>These compatibility flags should aid in the recreation of keys if an implementation loses backwards-compatibility.</para>
+		/// </remarks>
+		[Flags]
+		public enum Compatibility
+		{
+			/// <summary>
+			/// A 0 flag is required.
+			/// </summary>
+			None = 0,
+			/// <summary>
+			/// For RSA, Q will be greater than P.
+			/// </summary>
+			/// <remarks>
+			/// <para>PyCryptodome behaviour.</para>
+			/// </remarks>
+			RSAPrimeQGreaterThanPrimeP = 1,
+			/// <summary>
+			/// All numbers coming out of the RNG will have the MSb forcefully set to 1.
+			/// </summary>
+			/// <remarks>
+			/// <para>PyCryptodome and OpenSSL behaviour.</para>
+			/// </remarks>
+			SetMostSignificantBit = 2,
+			/// <summary>
+			/// All numbers coming out of the RNG will have the second-MSb forcefully set to 1.
+			/// </summary>
+			SetSecondMostSignificantBit = 4,
+			/// <summary>
+			/// Any numbers without the second-MSb set to 1 will be rejected.
+			/// </summary>
+			RequireSecondMostSignificantBit = 8,
+			/// <summary>
+			/// All numbers coming out of the RNG will have the LSb forcefully set to 1.
+			/// </summary>
+			/// <remarks>
+			/// <para>PyCryptodome and OpenSSL behaviour.</para>
+			/// </remarks>
+			SetLeastSignificantBit = 16,
+			/// <summary>
+			/// The generation of prime numbers and witness numbers will use the RNG, and threading will be disabled.
+			/// </summary>
+			/// <remarks>
+			/// <para>PyCryptodome behaviour since PyCryptodome v3.10.0.</para>
+			/// </remarks>
+			UseSuppliedRNGForPrimeWitnessNumbers = 32,
+			/// <summary>
+			/// PyCryptodome's number of Miller-Rabin rounds for an E-30 error level will be used.
+			/// </summary>
+			UsePyCryptodomeMillerRabinRounds = 64,
+			/// <summary>
+			/// The smallest number of bases will be used for Miller-Rabin, if there is a known set of bases that can be used for a deterministic test.
+			/// </summary>
+			UseDeterministicMillerRabinForSmallNumbers = 128,
+			/// <summary>
+			/// Deterministic Miller-Rabin will be performed for large numbers (very slow).
+			/// </summary>
+			UseDeterministicMillerRabinForLargeNumbers = 256,
+			/// <summary>
+			/// Miller-Rabin testing will be conducted.
+			/// </summary>
+			PerformMillerRabinTesting = 512,
+			/// <summary>
+			/// Lucas testing will be conducted.
+			/// </summary>
+			PerformLucasTesting = 1024,
+			/// <summary>
+			/// All Fermat numbers will be considered not-prime.
+			/// </summary>
+			RejectFermatNumbers = 2048,
+			/// <summary>
+			/// All Mersenne numbers will be considered not-prime.
+			/// </summary>
+			RejectMersenneNumbers = 4096,
+
+			// Notes
+			// -----
+			// PyCryptodome compatibility flag combinations
+			// Bugfix commits that didn't change the functionality of the maths were skipped over in my code comparisons
+
+			// PyCryptodome version v3.20.0 looks like it has had no relevant changes since v3.10.0
+			// 
+			// Crypto.Math.Primality
+			//   * Last bip85 breaking change was commit cb3f5dd639b88d91c3a55000715cf50b566bd504, 2021-01-04, first included in tag v3.10.0
+			//     * Commit message states "miller_rabin_test() was not using the provided random source"
+			//   * Previous real change was commit cd7f0128b6e5608911b82cecc0a0da19cdcb5db9, 2018-11-04, first included in tag v3.7.1x
+			/// <summary>
+			/// Flag combination for compatibility with PyCryptodome v3.10.0.
+			/// </summary>
+			PyCryptodomeVersion003010000 =
+				RSAPrimeQGreaterThanPrimeP |
+				SetMostSignificantBit |
+				SetLeastSignificantBit |
+				UseSuppliedRNGForPrimeWitnessNumbers |
+				UsePyCryptodomeMillerRabinRounds |
+				PerformMillerRabinTesting |
+				PerformLucasTesting,
+
+			// Crypto.Math._IntegerBase
+			//   * No breaking changes since at least commit fa933ab29ec66151f0e6f937cf998e8c27bfa075, 2018-11-23, first included in tag v3.7.1x
+			// Crypto.Math._IntergerGMP
+			//   * Last real change was commit cd7f0128b6e5608911b82cecc0a0da19cdcb5db9, 2018-11,04, first included in tag v3.7.1x
+			// Crypto.Random (see also: Crypto.Math.Primality)
+			//   * Last real change was commit cd7f0128b6e5608911b82cecc0a0da19cdcb5db9, 2018-11-04, first included in tag v3.7.1x
+			// Crypto.Math._IntegerNative
+			//   * No breaking changes since at least commit 7b1249e71812f276926b27505524b9102a09f421, 2018-01-21, first included in tag v3.4.8x
+			// Crypto.PublicKey.RSA.generate()
+			//   * Last real change was commit 1f3c1b4fcc2b21fc2f5e5f1519c3bd2c1814efda, 2015-03-11, first included in tag v3.1
+			/// <summary>
+			/// Flag combination for compatibility with PyCryptodome v3.7.1x.
+			/// </summary>
+			PyCryptoDomeVersion003007000x =
+				RSAPrimeQGreaterThanPrimeP |
+				SetMostSignificantBit |
+				SetLeastSignificantBit |
+				UsePyCryptodomeMillerRabinRounds |
+				PerformMillerRabinTesting |
+				PerformLucasTesting,
+
+			/// <summary>
+			/// Flag combination for compatibility with the bip85 Python reference implementation at the time RSA key generation tests were added.
+			/// </summary>
+			/// <remarks>
+			/// <para><see href="https://github.com/ethankosakovsky/bip85/commit/7892bbddc51f498d1a693cca3f6d642b75cb3373">GitHub/ethankosakovsky/bip85 commit 7892bbddc51f498d1a693cca3f6d642b75cb3373</see>, 2021-03-13, not yet in a tag/release.</para>
+			/// </remarks>
+			BIP85 = PyCryptodomeVersion003010000,
+
+			/// <summary>
+			/// Flag combination to reject primes that can be generated using well-known formulae, for being too weak.
+			/// </summary>
+			RejectWeakPrimes = RejectFermatNumbers |
+				RejectMersenneNumbers,
+
+			/// <summary>
+			/// Flag combination if BIP85 compatibility is not required.
+			/// </summary>
+			/// <remarks>
+			/// <para>WARNING: This flag combination has not yet been finalised and is subject to breaking changes.</para>
+			/// </remarks>
+			Default = UsePyCryptodomeMillerRabinRounds |
+				//UseDeterministicMillerRabinForSmallNumbers |
+				PerformMillerRabinTesting |
+				PerformLucasTesting |
+				RejectWeakPrimes
+		}
+	}
+}
---
+++ Utils/Maths/BigPrime.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Security.Cryptography;
+using uk.JohnCook.dotnet.LTOEncryptionManager.Utils.Algorithms;
+
+namespace uk.JohnCook.dotnet.LTOEncryptionManager.Utils.Maths
+{
+	/// <summary>
+	/// A wrapper class for a <see cref="BigInteger"/> object (referenced in <see cref="BigPrime.Value"/>) that extends <see cref="BigInteger"/>
+	///  with additional properties related to primality testing.
+	/// </summary>
+	public partial class BigPrime : IComparable<BigPrime>, IComparable<BigInteger>
+	{
+		/// <summary>
+		/// The first 100 prime numbers.
+		/// </summary>
+		public static readonly Collection<uint> First100Primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541];
+
+		/// <summary>
+		/// A random <see cref="int"/> (per thread) that is combined with <see cref="Value"/>'s hash code in <see cref="GetHashCode"/>.
+		/// </summary>
+		/// <remarks>
+		/// <para><see cref="BigInteger.GetHashCode"/> returns the same value (per thread?) for all <see cref="BigInteger"/> objects that have identical values.</para>
+		/// <para><see cref="BigPrime"/> objects have properties based entirely on the mathematical properties of the value of <see cref="Value"/>.</para>
+		/// <para>By combining this 'prefix' with the hash code for <see cref="Value"/>, all <see cref="BigPrime"/> objects with identical underlying
+		///  <see cref="BigInteger"/>s will have identical (per thread) <see cref="GetHashCode"/> return values.</para>
+		/// </remarks>
+		private static readonly int hashCodePrefix = RandomNumberGenerator.GetInt32(int.MinValue, int.MaxValue);
+
+		/// <summary>
+		/// The underlying <see cref="BigInteger"/> of this <see cref="BigPrime"/>.
+		/// </summary>
+		public BigInteger Value { get; private set; }
+
+		/// <inheritdoc cref="BigInteger.IsEven"/>
+		public bool IsEven => Value.IsEven;
+
+		/// <summary>
+		/// Indicates whether the value of the current <see cref="BigPrime.Value"/> property is an odd number.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> if the value of the <see cref="BigPrime.Value"/> property is an odd number; otherwise, <see langword="false"/>.
+		/// </returns>
+		public bool IsOdd => !Value.IsEven;
+
+		/// <inheritdoc cref="BigInteger.IsZero"/>
+		public bool IsZero => Value.IsZero;
+
+		/// <inheritdoc cref="BigInteger.IsOne"/>
+		public bool IsOne => Value.IsOne;
+
+		/// <inheritdoc cref="BigInteger.IsPowerOfTwo"/>
+		public bool IsPowerOfTwo => Value.IsPowerOfTwo;
+
+		/// <inheritdoc cref="BigInteger.GetBitLength"/>
+		public long BitLength => Value.GetBitLength();
+
+		/// <inheritdoc cref="BigInteger.GetByteCount(bool)"/>
+		public int UnsignedByteCount => Value.GetByteCount(true);
+
+		/// <inheritdoc cref="BigInteger.GetByteCount(bool)"/>
+		public int SignedByteCount => Value.GetByteCount(false);
+
+		bool? _isPerfectSquare;
+		/// <summary>
+		/// Indicates whether the value of the <see cref="BigPrime.Value"/> property is a perfect square.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> if the value of the <see cref="BigPrime.Value"/> property is a perfect square; otherwise, <see langword="false"/>.
+		/// </returns>
+		public bool IsPerfectSquare
+		{
+			get
+			{
+				_isPerfectSquare ??= CheckPerfectSquare();
+				return _isPerfectSquare.GetValueOrDefault(false);
+			}
+		}
+
+		/// <summary>
+		/// Indicates whether a factor has been found for the value of the <see cref="BigPrime.Value"/> property.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> if a factor has been found for value of the <see cref="BigPrime.Value"/> property; otherwise, <see langword="null"/>.
+		/// </returns>
+		public bool? HasFactor { get; private set; }
+
+		bool? _isSmallPrime;
+		/// <summary>
+		/// Indicates whether the value of the <see cref="BigPrime.Value"/> property is a small prime.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> if the value of the <see cref="BigPrime.Value"/> property is contained in <see cref="BigPrime.First100Primes"/>;
+		///  otherwise, <see langword="false"/>.
+		/// </returns>
+		public bool IsSmallPrime
+		{
+			get
+			{
+				_isSmallPrime ??= CheckSmallPrime();
+				return _isSmallPrime.GetValueOrDefault(false);
+			}
+		}
+
+		bool? _isFermatNumber;
+		/// <summary>
+		/// Indicates whether the value of the <see cref="Value"/> property is a Fermat number.
+		/// </summary>
+		/// <remarks>
+		/// <para>Note: This property does not indicate Fermat primes.</para>
+		/// </remarks>
+		public bool IsFermatNumber
+		{
+			get
+			{
+				_isFermatNumber ??= Value.IsFermatNumber();
+				return _isFermatNumber.GetValueOrDefault(false);
+			}
+		}
+
+		bool? _isMersenneNumber;
+		/// <summary>
+		/// Indicates whether the value of the <see cref="Value"/> property is a Mersenne number.
+		/// </summary>
+		/// <remarks>
+		/// <para>Note: This property does not indicate Mersenne primes.</para>
+		/// </remarks>
+		public bool IsMersenneNumber
+		{
+			get
+			{
+				_isMersenneNumber ??= Value.IsMersenneNumber();
+				return _isMersenneNumber.GetValueOrDefault(false);
+			}
+		}
+
+		/// <summary>
+		/// Indicates whether the value of the <see cref="Value"/> property is possibly a weak prime.
+		/// </summary>
+		/// <returns>
+		/// <para><see langword="true"/> if any of the following are true:
+		///   <see cref="IsSmallPrime"/>,
+		///   <see cref="IsFermatNumber"/>,
+		///   <see cref="IsMersenneNumber"/>; otherwise <see langword="false"/>.</para>
+		/// </returns>
+		/// <remarks>
+		/// <para>Note: This property does not indicate primality.</para>
+		/// </remarks>
+		public bool IsPossiblyWeakPrime => IsSmallPrime || IsFermatNumber || IsMersenneNumber;
+
+		/// <summary>
+		/// Flags that indicated, at the time the current <see cref="BigPrime"/> object was created, how primality and other tests should be conducted
+		///  on the value of the <see cref="BigPrime.Value"/> property.
+		/// </summary>
+		public StandardRSA.Compatibility CompatibilityFlags { get; private set; }
+
+		/// <inheritdoc cref="BigPrime.BigPrime(BigInteger, StandardRSA.Compatibility)"/>
+		public BigPrime(byte number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Initialise(number, compatibilityFlags);
+		}
+
+		/// <inheritdoc cref="BigPrime.BigPrime(BigInteger, StandardRSA.Compatibility)"/>
+		public BigPrime(ushort number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Initialise(number, compatibilityFlags);
+		}
+
+		/// <inheritdoc cref="BigPrime.BigPrime(BigInteger, StandardRSA.Compatibility)"/>
+		public BigPrime(uint number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Initialise(number, compatibilityFlags);
+		}
+
+		/// <inheritdoc cref="BigPrime.BigPrime(BigInteger, StandardRSA.Compatibility)"/>
+		public BigPrime(ulong number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Initialise(number, compatibilityFlags);
+		}
+
+		/// <summary>
+		/// Create a new <see cref="BigPrime"/> object based on <paramref name="number"/>.
+		/// </summary>
+		/// <param name="number">The number that will be referenced by <see cref="BigPrime.Value"/>.</param>
+		/// <param name="compatibilityFlags">Flags that indicate how primality and other tests should be conducted on the <see cref="BigPrime"/> object.</param>
+		public BigPrime(BigInteger number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Initialise(number, compatibilityFlags);
+		}
+
+		private void Initialise(BigInteger number, StandardRSA.Compatibility compatibilityFlags)
+		{
+			Value = number;
+			CompatibilityFlags = compatibilityFlags;
+			if (IsEven && number > 2)
+			{
+				HasFactor = true;
+			}
+		}
+
+		private bool CheckSmallPrime()
+		{
+			return BigPrime.First100Primes.Count > 0 &&
+				Value <= BigPrime.First100Primes[^1] &&
+				BigPrime.First100Primes.Contains((uint)Value);
+		}
+
+		private bool CheckPerfectSquare()
+		{
+			return Value.IsPerfectSquare();
+		}
+
+		/// <summary>
+		/// Checks whether <paramref name="factor"/> can be used to determine if
+		///  <see cref="HasFactor"/> should be <see langword="true"/>.
+		/// </summary>
+		/// <param name="factor">A possible factor of <see cref="Value"/>.</param>
+		/// <remarks>
+		/// <para>If <see cref="HasFactor"/> is already <see langword="true"/>, this method
+		///  does nothing.</para>
+		/// <para>If the GCD of <see cref="Value"/> and <paramref name="factor"/> is not 1,
+		///  <see cref="HasFactor"/> will be set to <see langword="true"/>.</para>
+		/// <para>If the GCD of <see cref="Value"/> and <paramref name="factor"/> is 1,
+		///  <see cref="HasFactor"/> will be set to <see langword="true"/> if <see cref="Value"/>
+		///  is wholly divisible by <paramref name="factor"/> with a quotient greater than 1.</para>
+		///  </remarks>
+		public void CheckFactor(BigInteger factor)
+		{
+			if (HasFactor.GetValueOrDefault(false))
+			{
+				return;
+			}
+			BigInteger gcd = BigInteger.GreatestCommonDivisor(factor, Value);
+			if (gcd != 1)
+			{
+				HasFactor = true;
+				return;
+			}
+			(BigInteger quotient, BigInteger remainder) = BigInteger.DivRem(Value, factor);
+			if (remainder == 0 && quotient > 1)
+			{
+				HasFactor = true;
+			}
+		}
+
+		/// <inheritdoc cref="BigInteger.CompareTo(BigInteger)"/>
+		public int CompareTo(BigPrime? other)
+		{
+			return Value.CompareTo(other?.Value);
+		}
+
+		/// <inheritdoc cref="object.Equals(object?)"/>
+		public override bool Equals(object? obj)
+		{
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+			else if (obj is null)
+			{
+				return false;
+			}
+			else
+			{
+				return obj is BigPrime && Value.Equals((obj as BigPrime)?.Value);
+			}
+		}
+
+		/// <summary>
+		/// Returns the hash code for the current <see cref="BigPrime"/> object.
+		/// </summary>
+		/// <returns>A 32-bit signed integer hash code.</returns>
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(hashCodePrefix, Value.GetHashCode());
+		}
+
+		/// <inheritdoc cref = "BigInteger.CompareTo(BigInteger)" />
+		public int CompareTo(BigInteger other)
+		{
+			return Value.CompareTo(other);
+		}
+
+		/// <summary>Defines an implicit conversion of a <see cref="BigPrime"/> to a <see cref="BigInteger"/>.</summary>
+		/// <param name="value">The value to convert to a <see cref="BigInteger"/>.</param>
+		/// <returns>An object that contains the value of the value parameter.</returns>
+		public static implicit operator BigInteger(BigPrime value)
+		{
+			ArgumentNullException.ThrowIfNull(value);
+			return value.Value;
+		}
+
+		/// <summary>
+		/// Gets the <see cref="BigInteger"/> object this <see cref="BigPrime"/> object references.
+		/// </summary>
+		/// <returns>The current <see cref="BigPrime"/> object's underlying <see cref="BigInteger"/> object (the <see cref="Value"/> property).</returns>
+		public BigInteger ToBigInteger()
+		{
+			return Value;
+		}
+
+		/// <inheritdoc cref="BigInteger.ToByteArray()"/>
+		public byte[] ToByteArray()
+		{
+			return Value.ToByteArray();
+		}
+
+		/// <inheritdoc cref="BigInteger.ToByteArray(bool, bool)"/>
+		public byte[] ToByteArray(bool isUnsigned = false, bool isBigEndian = false)
+		{
+			return Value.ToByteArray(isUnsigned, isBigEndian);
+		}
+
+		/// <inheritdoc cref="BigInteger.operator==(BigInteger, BigInteger)"/>
+		public static bool operator ==(BigPrime left, BigPrime right)
+		{
+			if (Equals(left?.Value, null))
+			{
+				return Equals(right?.Value, null);
+			}
+
+			return left.Value.Equals(right?.Value);
+		}
+
+		/// <inheritdoc cref="BigInteger.operator!=(BigInteger, BigInteger)"/>
+		public static bool operator !=(BigPrime left, BigPrime right)
+		{
+			return !(left == right);
+		}
+
+		/// <inheritdoc cref="BigInteger.operator&lt;(BigInteger, BigInteger)"/>
+		public static bool operator <(BigPrime left, BigPrime right)
+		{
+			return left is null ? right is not null : left.CompareTo(right) < 0;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator&lt;=(BigInteger, BigInteger)"/>
+		public static bool operator <=(BigPrime left, BigPrime right)
+		{
+			return left is null || left.CompareTo(right) <= 0;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator&gt;(BigInteger, BigInteger)"/>
+		public static bool operator >(BigPrime left, BigPrime right)
+		{
+			return left is not null && left.CompareTo(right) > 0;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator&gt;=(BigInteger, BigInteger)"/>
+		public static bool operator >=(BigPrime left, BigPrime right)
+		{
+			return left is null ? right is null : left.CompareTo(right) >= 0;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator*(BigInteger, BigInteger)"/>
+		public static BigInteger operator *(BigPrime left, BigInteger right)
+		{
+			return Multiply(left, right);
+		}
+
+		/// <inheritdoc cref="BigInteger.Multiply(BigInteger, BigInteger)"/>
+		public static BigInteger Multiply(BigPrime left, BigInteger right)
+		{
+			ArgumentNullException.ThrowIfNull(left);
+			ArgumentNullException.ThrowIfNull(right);
+			return left.Value * right;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator+(BigInteger, BigInteger)"/>
+		public static BigInteger operator +(BigPrime left, BigInteger right)
+		{
+			return Add(left, right);
+		}
+
+		/// <inheritdoc cref="BigInteger.Add(BigInteger, BigInteger)"/>
+		public static BigInteger Add(BigPrime left, BigInteger right)
+		{
+			ArgumentNullException.ThrowIfNull(left);
+			ArgumentNullException.ThrowIfNull(right);
+			return left.Value + right;
+		}
+
+		/// <inheritdoc cref="BigInteger.operator-(BigInteger, BigInteger)"/>
+		public static BigInteger operator -(BigPrime left, BigInteger right)
+		{
+			return Subtract(left, right);
+		}
+
+		/// <inheritdoc cref="BigInteger.Subtract(BigInteger, BigInteger)"/>
+		public static BigInteger Subtract(BigPrime left, BigInteger right)
+		{
+			ArgumentNullException.ThrowIfNull(left);
+			ArgumentNullException.ThrowIfNull(right);
+			return left.Value - right;
+		}
+	}
+}
